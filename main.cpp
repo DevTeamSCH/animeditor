e624ccbbe1ae628bc7f5204cb624233b2d9be6b3
Levente Lenart <leviiibog@gmail.com>
2018-03-02 14:57:04 +0100

    Create Qt5 project

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+
+#include "ui/mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
