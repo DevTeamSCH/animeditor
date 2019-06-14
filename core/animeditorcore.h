4bf4bd0a736d535189ed31801ccd5109577c4d69
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2019-06-14 19:40:58 +0200

    Create initial project structure
    /core, non gui classes
    /gui, gui classes components and main application

---
+++ core/animeditorcore.h
@@ -0,0 +1,10 @@
+#ifndef ANIMEDITORCORE_H
+#define ANIMEDITORCORE_H
+
+#include "core_global.h"
+class CORESHARED_EXPORT AnimEditorCore {
+ public:
+  AnimEditorCore();
+};
+
+#endif  // ANIMEDITORCORE_H
