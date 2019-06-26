1018e911a5167959393503abc6191b8210c7968b
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2019-06-26 11:14:29 +0200

    Add basic ui for testing

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -1,9 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "animationmodel.h"
+#include "framedelegate.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+
+  ui->tableView->setModel(new SchMatrix::AnimationModel(this));
+  ui->tableView->setItemDelegate(new SchMatrix::FrameDelegate(this));
 }
 
 MainWindow::~MainWindow() { delete ui; }
