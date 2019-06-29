dd79a46e6497b4c8799e5c959c9a9a24306360ad
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2019-06-29 17:57:19 +0200

    Update layer
    Fix non const functions
    Add convenience functions

--- core/layer.cpp
+++ core/layer.cpp
@@ -7,7 +7,29 @@ namespace SchMatrix {
 Layer::Layer(QObject *parent, QString name, int zOrder)
     : QSequentialAnimationGroup(parent), name(name), zOrder(zOrder) {}
 
-QList<Keyframe *> Layer::keyframes() {
+QList<QAbstractAnimation *> Layer::animations() const {
+  QList<QAbstractAnimation *> anims;
+
+  for (int i = 0; i < animationCount(); ++i) {
+    anims.append(animationAt(i));
+  }
+
+  return anims;
+}
+
+QList<QPauseAnimation *> Layer::pauses() const {
+  QList<QPauseAnimation *> pauseAnims;
+
+  for (int i = 0; i < animationCount(); ++i) {
+    auto anim = animationAt(i);
+    if (qobject_cast<QPauseAnimation *>(anim))
+      pauseAnims.append(static_cast<QPauseAnimation *>(anim));
+  }
+
+  return pauseAnims;
+}
+
+QList<Keyframe *> Layer::keyframes() const {
   QList<Keyframe *> keyframes;
 
   for (int i = 0; i < animationCount(); ++i) {
@@ -19,7 +41,7 @@ QList<Keyframe *> Layer::keyframes() {
   return keyframes;
 }
 
-Keyframe *Layer::currentKeyframe() {
+Keyframe *Layer::currentKeyframe() const {
   auto anim = currentAnimation();
 
   if (qobject_cast<Keyframe *>(anim))
@@ -28,7 +50,7 @@ Keyframe *Layer::currentKeyframe() {
     return static_cast<Keyframe *>(animationAt(indexOfAnimation(anim) - 1));
 }
 
-Keyframe *Layer::nextKeyframe() {
+Keyframe *Layer::nextKeyframe() const {
   if (animationCount() < 2) return nullptr;
 
   auto keys = keyframes();
@@ -39,7 +61,7 @@ Keyframe *Layer::nextKeyframe() {
   return keys[idx + 1];
 }
 
-Keyframe *Layer::prevKeyframe() {
+Keyframe *Layer::prevKeyframe() const {
   if (animationCount() < 2) return nullptr;
 
   auto keys = keyframes();
@@ -50,12 +72,26 @@ Keyframe *Layer::prevKeyframe() {
   return keys[idx - 1];
 }
 
-QString Layer::getName() { return name; }
+QString Layer::getName() const { return name; }
 
-int Layer::getZOrder() { return zOrder; };
+int Layer::getZOrder() const { return zOrder; };
 
 void Layer::setName(const QString &newName) { name = newName; }
 
-void Layer::setZOrder(const int &order) { zOrder = order; };
+void Layer::setZOrder(const int &order) { zOrder = order; }
+
+void Layer::deleteEmptyPauses() {
+  for (auto p : pauses()) {
+    if (p->duration() == 0) delete p;
+  }
+}
+
+QAbstractAnimation *Layer::animationAtMsec(int msec) const {
+  auto root = static_cast<QAbstractAnimation *>(parent());
+
+  root->setCurrentTime(msec);
+
+  return currentAnimation();
+};
 
 }  // namespace SchMatrix
--- core/layer.h
+++ core/layer.h
@@ -16,15 +16,22 @@ class CORESHARED_EXPORT Layer : public QSequentialAnimationGroup {
  public:
   explicit Layer(QObject *parent = nullptr, QString name = "layer",
                  int zOrder = 0);
-  QList<Keyframe *> keyframes();
-  Keyframe *currentKeyframe();
-  Keyframe *nextKeyframe();
-  Keyframe *prevKeyframe();
+  QList<QAbstractAnimation *> animations() const;
+  QList<QPauseAnimation *> pauses() const;
+  QList<Keyframe *> keyframes() const;
+  Keyframe *currentKeyframe() const;
+  Keyframe *nextKeyframe() const;
+  Keyframe *prevKeyframe() const;
 
-  QString getName();
-  int getZOrder();
+  QString getName() const;
+  int getZOrder() const;
   void setName(const QString &name);
   void setZOrder(const int &zOrder);
+
+  // pause garbage collection
+  void deleteEmptyPauses();
+
+  QAbstractAnimation *animationAtMsec(int msec) const;
 
  private:
   QString name;
