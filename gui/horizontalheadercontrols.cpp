c552425ec889ab10be4a587a4bfa37bd7811b84f
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2019-07-26 11:47:01 +0200

    :construction: WIP HorizontalHeaderControls

--- gui/horizontalheadercontrols.cpp
+++ gui/horizontalheadercontrols.cpp
@@ -1,13 +1,121 @@
 #include "horizontalheadercontrols.h"
 #include "ui_horizontalheadercontrols.h"
 
+#include <QDebug>
+#include <QSignalBlocker>
+#include "config.h"
+#include "horizontalheader.h"
+
 namespace SchMatrix {
 
-HorizontalHeaderControls::HorizontalHeaderControls(QWidget *parent)
-    : QWidget(parent), ui(new Ui::HorizontalHeaderControls) {
+HorizontalHeaderControls::HorizontalHeaderControls(QHeaderView *parent)
+    : QWidget(parent),
+      ui(new Ui::HorizontalHeaderControls),
+      timeLine(SchMatrix::frameLength, this),
+      parentHeader(static_cast<SchMatrix::HorizontalHeader *>(parent)),
+      defaultSectionSize(10) {
   ui->setupUi(this);
+
+  timeLine.setUpdateInterval(SchMatrix::fps);
+  timeLine.setFrameRange(0, 1);
 }
 
 HorizontalHeaderControls::~HorizontalHeaderControls() { delete ui; }
 
+void HorizontalHeaderControls::setModel(AnimationModel *model) {
+  animModel = model;
+
+  connect(animModel, SIGNAL(frameChanged(int)), this, SLOT(updateFrame(int)));
+  connect(animModel, SIGNAL(timelineChanged()), this, SLOT(updateTimeline()));
+}
+
+void HorizontalHeaderControls::updateFrame(int frame) {
+  // Prevent recursion
+  QSignalBlocker b(ui->currentFrame);
+  QSignalBlocker b2(ui->ellapsedTime);
+
+  // + 1 because spinner starts at 1 not 0
+  ui->currentFrame->setValue(frame + 1);
+
+  ui->ellapsedTime->setValue((double)animModel->getTime() / 1000);
+}
+
+void HorizontalHeaderControls::updateTimeline() {
+  auto lastFrame = animModel->getLastFrame();
+
+  ui->currentFrame->setMaximum(lastFrame + 1);
+  timeLine.setFrameRange(0, lastFrame);
+}
+
 }  // namespace SchMatrix
+
+void SchMatrix::HorizontalHeaderControls::on_resetZoom_clicked() {
+  ui->zoomSlider->setValue(defaultSectionSize);
+}
+
+void SchMatrix::HorizontalHeaderControls::on_zoomIn_clicked() {
+  ui->zoomSlider->setValue(ui->zoomSlider->value() + 5);
+}
+
+void SchMatrix::HorizontalHeaderControls::on_zoomSlider_valueChanged(
+    int value) {
+  parentHeader->setDefaultSectionSize(value);
+}
+
+void SchMatrix::HorizontalHeaderControls::on_zoomOut_clicked() {
+  ui->zoomSlider->setValue(ui->zoomSlider->value() - 5);
+}
+
+void SchMatrix::HorizontalHeaderControls::on_currentFrame_valueChanged(
+    int val) {
+  auto frame = val - 1;
+  timeLine.setCurrentTime(frame);
+
+  // Prevent recursion
+  QSignalBlocker b(animModel);
+
+  animModel->setFrame(frame);
+  parentHeader->updateFrame(frame);
+}
+
+void SchMatrix::HorizontalHeaderControls::on_ellapsedTime_valueChanged(
+    double time) {
+  auto intTime = qRound(time);
+  auto frame = (intTime == 1) ? 0 : intTime - 1;
+
+  timeLine.setCurrentTime(frame);
+}
+
+void SchMatrix::HorizontalHeaderControls::on_frameRate_valueChanged(int fps) {
+  timeLine.setUpdateInterval(fps);
+  SchMatrix::fps = fps;
+  SchMatrix::frameLength = 1000 / SchMatrix::fps;
+
+  emit fpsChanged(fps);
+}
+
+void SchMatrix::HorizontalHeaderControls::on_prevKeyframe_clicked() {}
+
+void SchMatrix::HorizontalHeaderControls::on_insertKeyframe_clicked() {}
+
+void SchMatrix::HorizontalHeaderControls::on_nextKeyframe_clicked() {}
+
+void SchMatrix::HorizontalHeaderControls::on_firstFrame_clicked() {}
+
+void SchMatrix::HorizontalHeaderControls::on_prevFrame_clicked() {}
+
+void SchMatrix::HorizontalHeaderControls::on_play_clicked() {
+  if (timeLine.state() == QTimeLine::Running) {
+    timeLine.setPaused(true);
+    ui->play->setIcon(QIcon(
+        ":/resources/breeze-icons/icons/actions/16/media-playback-start.svg"));
+  } else {
+    timeLine.start();
+    ui->play->setIcon(QIcon(
+        ":/resources/breeze-icons/icons/actions/16/media-playback-pause.svg"));
+  }
+}
+
+void SchMatrix::HorizontalHeaderControls::on_nextFrame_clicked() {}
+
+void SchMatrix::HorizontalHeaderControls::on_lastFrame_clicked() {}
--- gui/horizontalheadercontrols.h
+++ gui/horizontalheadercontrols.h
@@ -1,10 +1,17 @@
 #ifndef HORIZONTALHEADERCONTROLS_H
 #define HORIZONTALHEADERCONTROLS_H
 
+#include <QHeaderView>
+#include <QTimeLine>
 #include <QWidget>
+#include "animationmodel.h"
 
 namespace Ui {
 class HorizontalHeaderControls;
+}
+
+namespace SchMatrix {
+class HorizontalHeader;
 }
 
 namespace SchMatrix {
@@ -13,11 +20,56 @@ class HorizontalHeaderControls : public QWidget {
   Q_OBJECT
 
  public:
-  explicit HorizontalHeaderControls(QWidget *parent = nullptr);
+  explicit HorizontalHeaderControls(QHeaderView *parent = nullptr);
   ~HorizontalHeaderControls();
+
+  void setModel(SchMatrix::AnimationModel *model);
+
+ public slots:
+  void updateFrame(int frame);
+
+ signals:
+  void fpsChanged(int fps);
+
+ private slots:
+  void updateTimeline();
+
+  void on_resetZoom_clicked();
+
+  void on_zoomIn_clicked();
+
+  void on_zoomSlider_valueChanged(int value);
+
+  void on_zoomOut_clicked();
+
+  void on_currentFrame_valueChanged(int arg1);
+
+  void on_ellapsedTime_valueChanged(double arg1);
+
+  void on_frameRate_valueChanged(int arg1);
+
+  void on_prevKeyframe_clicked();
+
+  void on_insertKeyframe_clicked();
+
+  void on_nextKeyframe_clicked();
+
+  void on_lastFrame_clicked();
+
+  void on_firstFrame_clicked();
+
+  void on_prevFrame_clicked();
+
+  void on_play_clicked();
+
+  void on_nextFrame_clicked();
 
  private:
   Ui::HorizontalHeaderControls *ui;
+  QTimeLine timeLine;
+  SchMatrix::HorizontalHeader *parentHeader;
+  int defaultSectionSize;
+  SchMatrix::AnimationModel *animModel;
 };
 
 }  // namespace SchMatrix
