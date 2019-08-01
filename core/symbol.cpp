9577f99ea2656a9da4af3db1753808caad89bae5
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2019-08-01 14:00:53 +0200

    :sparkles: Add item management in Symbol

--- core/symbol.cpp
+++ core/symbol.cpp
@@ -2,6 +2,42 @@
 
 namespace SchMatrix {
 
-Symbol::Symbol(QGraphicsItem *parent) : QGraphicsWidget(parent) {}
+Symbol::Symbol(QList<QGraphicsWidget*> items, QGraphicsScene* scene,
+               QGraphicsItem* parent)
+    : QGraphicsWidget(parent), animModel(scene, this), itemGroup(this) {
+  for (auto i : items) {
+    itemGroup.addToGroup(i);
+  }
+}
+
+Symbol::Symbol(QGraphicsWidget* item, QGraphicsScene* scene,
+               QGraphicsItem* parent)
+    : QGraphicsWidget(parent), animModel(scene, this), itemGroup(this) {
+  itemGroup.addToGroup(item);
+}
+
+Symbol::~Symbol() {
+  for (auto i : itemGroup.childItems()) {
+    itemGroup.removeFromGroup(i);
+  }
+}
+
+void Symbol::addItem(QGraphicsWidget* item) { itemGroup.addToGroup(item); }
+
+void Symbol::removeItem(QGraphicsWidget* item) {
+  itemGroup.removeFromGroup(item);
+}
+
+QList<QGraphicsWidget*> Symbol::items() {
+  QList<QGraphicsWidget*> list;
+
+  for (auto i : itemGroup.childItems()) {
+    list.append(static_cast<QGraphicsWidget*>(i));
+  }
+
+  return list;
+}
+
+AnimationModel* Symbol::getAnimationModel() { return &animModel; }
 
 }  // namespace SchMatrix
--- core/symbol.h
+++ core/symbol.h
@@ -1,7 +1,11 @@
 #ifndef SYMBOL_H
 #define SYMBOL_H
 
+#include <QGraphicsItemGroup>
+#include <QGraphicsScene>
 #include <QGraphicsWidget>
+#include <QList>
+#include "animationmodel.h"
 
 namespace SchMatrix {
 
@@ -9,7 +13,21 @@ class Symbol : public QGraphicsWidget {
   Q_OBJECT
 
  public:
-  explicit Symbol(QGraphicsItem *parent = nullptr);
+  explicit Symbol(QList<QGraphicsWidget*> items, QGraphicsScene*,
+                  QGraphicsItem* parent = nullptr);
+  explicit Symbol(QGraphicsWidget* item, QGraphicsScene*,
+                  QGraphicsItem* parent = nullptr);
+  ~Symbol();
+
+  void addItem(QGraphicsWidget* item);
+  void removeItem(QGraphicsWidget* item);
+
+  QList<QGraphicsWidget*> items();
+  AnimationModel* getAnimationModel();
+
+ private:
+  AnimationModel animModel;
+  QGraphicsItemGroup itemGroup;
 };
 
 }  // namespace SchMatrix
