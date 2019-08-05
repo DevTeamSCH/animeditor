92854b3cb6cfcf957d309d182f68b0b2714f2f54
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2019-08-05 10:55:37 +0200

    :sparkles: Add strokeColor, fillColor
    StrokeColor for Pen color
    FillColor for Brush color

--- core/graphicswidget.cpp
+++ core/graphicswidget.cpp
@@ -5,4 +5,18 @@ namespace SchMatrix {
 GraphicsWidget::GraphicsWidget(QGraphicsItem *parent, Qt::WindowFlags wFlags)
     : QGraphicsWidget(parent, wFlags) {}
 
+QColor GraphicsWidget::getStrokeColor() const { return strokeColor; }
+
+void GraphicsWidget::setStrokeColor(const QColor &value) {
+  strokeColor = value;
+  emit strokeColorChanged(value);
+}
+
+QColor GraphicsWidget::getFillColor() const { return fillColor; }
+
+void GraphicsWidget::setFillColor(const QColor &value) {
+  fillColor = value;
+  emit fillColorChanged(value);
+}
+
 }  // namespace SchMatrix
--- core/graphicswidget.h
+++ core/graphicswidget.h
@@ -1,6 +1,7 @@
 #ifndef GRAPHICSWIDGET_H
 #define GRAPHICSWIDGET_H
 
+#include <QColor>
 #include <QGraphicsWidget>
 #include "core_global.h"
 
@@ -8,10 +9,26 @@ namespace SchMatrix {
 
 class CORESHARED_EXPORT GraphicsWidget : public QGraphicsWidget {
   Q_OBJECT
+  Q_PROPERTY(QColor strokeColor READ getStrokeColor WRITE setStrokeColor NOTIFY
+                 strokeColorChanged)
+  Q_PROPERTY(QColor fillColor READ getFillColor WRITE setFillColor NOTIFY
+                 fillColorChanged)
 
  public:
   explicit GraphicsWidget(QGraphicsItem *parent = nullptr,
                           Qt::WindowFlags wFlags = Qt::WindowFlags());
+
+  QColor getStrokeColor() const;
+  void setStrokeColor(const QColor &value);
+  QColor getFillColor() const;
+  void setFillColor(const QColor &value);
+
+ signals:
+  void strokeColorChanged(QColor color);
+  void fillColorChanged(QColor color);
+
+ protected:
+  QColor strokeColor, fillColor;
 };
 
 }  // namespace SchMatrix
